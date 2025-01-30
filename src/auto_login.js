bd7cdf42b3d341626ced2f755cfb9cdf4b1392da
pedroituassu <pedroituassu@proton.me>
2025-01-30 11:28:51 -0400

    tudo funcionando, faltam apenas as imagens no readme

--- src/auto_login.js
+++ src/auto_login.js
@@ -4,8 +4,9 @@ const auto_login = (email, senha) => {
     document.getElementById("formLogin").submit()
 }
 
-browser.storage.local.get(["email", "senha"]).then((data) => {
+browser.storage.local.get(["email", "senha", "toggled"]).then((data) => {
     let email = data.email;
     let senha = data.senha;
-    auto_login(email, senha)
+    let autoLogin = data.toggled;
+    if (autoLogin) auto_login(email, senha);
 })
--- src/popup/toggle.js
+++ src/popup/toggle.js
@@ -1,27 +1,38 @@
 const toggleButton = document.getElementById("toggleButton");
 const toggleCircle = document.getElementById("toggleCircle");
-let toggled = false;
+let toggled = true;
 
+console.log("Toggle state when starting:", toggled)
 // Retrieve the saved toggle state when the popup loads
 browser.storage.local.get("toggled").then((result) => {
     if (result.toggled !== undefined) {
         toggled = result.toggled;
+        console.log("Toggle state after getting storage:", toggled)
         updateToggleUI();
     }
 });
 
 // Toggle functionality
 toggleButton.addEventListener("click", () => {
+    console.log("Toggle state before click:", toggled);
     toggled = !toggled;
     updateToggleUI();
     // Save the toggle state to storage
     browser.storage.local.set({ toggled: toggled }).then(() => {
-        console.log("Toggle state saved:", toggled);
+        console.log("Toggle state after click:", toggled);
     });
 });
 
 // Update the UI based on the toggle state
 function updateToggleUI() {
-    toggleButton.classList.toggle("bg-green-600/20", toggled);
-    toggleCircle.classList.toggle("translate-x-6", toggled);
+    if (toggled) {
+        toggleButton.classList.add("bg-green-600");
+        toggleCircle.classList.add("translate-x-6");
+        toggleButton.classList.remove("bg-green-600/20");
+    } else {
+        toggleButton.classList.add("bg-green-600/20");
+        toggleButton.classList.remove("bg-green-600");
+        toggleCircle.classList.remove("translate-x-6");
+    }
+    console.log("Toggle state after updateToggleUI:", toggled);
 }
